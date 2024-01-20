2f15e7b386021e7b188af74c7c1966d874287080
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-20 09:29:25 -0500

    Modified Home page -> src/app/home; Added Products methods -> src/app/services

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private _http = inject(HttpClient);
+  private urlBase: string = 'https://fakestoreapi.com/products';
+
+  getProducts():Observable<any>{
+    return this._http.get(this.urlBase);
+  }
+
+  getProduct(id: number):Observable<any>{
+    return this._http.get(`${this.urlBase}/${id}`)
+  }
+
 }
