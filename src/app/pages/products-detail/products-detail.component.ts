0898e84945629f339f2059fef8fdb715380f83ec
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-20 12:03:56 -0500

    Modified products-details component, html and css -> src/app/pages/products-detail

--- src/app/pages/products-detail/products-detail.component.ts
+++ src/app/pages/products-detail/products-detail.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-detail',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './products-detail.component.html',
   styleUrl: './products-detail.component.css'
 })
-export class ProductsDetailComponent {
+export class ProductsDetailComponent implements OnInit{
+
+  loading:boolean = true;
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService);
+  public product?: IProduct;
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      this._apiService.getProduct(params['id']).subscribe((data: IProduct) => {
+        this.product = data
+        this.loading = false;
+      });
+    });
+  }
 
 }
