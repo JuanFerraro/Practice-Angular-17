31aebed7349d11341df90fe972e6b9666381241b
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-20 11:05:05 -0500

    Added products methods and html -> src/app/pages/products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-products',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  private _apiService = inject(ApiService);
+  productList: any[] = []
+
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe((data: any[]) =>{
+      this.productList = data;
+    });
+  }
 
 }
